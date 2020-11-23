27dea5ee59f7534e823bf2b33802bb18e5f15c09
PilarOrtegaM <gortegamontes41@correo.unicordoba.edu.co>
2020-11-23 15:08:31 -0500

    servidor app.js, index.html y bin/Rooms.js
    Se montó un servidor básico, Creamos el archivo que contendrá las rutinas de administración y el archivo html que es el cliente.

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const shortid = require('shortid');
 
+//Sockets
+io.sockets.on('connect', (socket) => {
+    console.log(`Un nuevo usuario con id ${socket.id} se ha conectado`)
+
+    var msg = `El usuario con id ${socket.id} se ha conectado`
+
+    socket.emit('connect', msg);
+
+    socket.on('setPing', () => { // se activa en el servidor el evento on con nombre setPing enviado desde el cliente
+        socket.emit('setPong', 'Esto es un pong'); //enviamos una respuesta al cliente con un nuevo nombre
+    });
+})
+
+//cargar la plantilla html del juego
 app.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-      });
-});
-
+//levantar el servidor
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
